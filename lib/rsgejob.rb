a76fc1f246192bc88b3dc5c18fa2da94a5dec4c7
Laura Schlittler <llschlit@mail.usf.edu>
2014-11-06 09:56:51 -0500

    Preliminary migration to slurm - rsgejob.rb and rsgejobs.rb now functional

--- lib/rsgejob.rb
+++ lib/rsgejob.rb
@@ -33,93 +33,121 @@ class RsgeJob
     # We're a valid job unless we run into some condition
     self.valid = true
 
+# TODO: figure out what is going on here...as I see it, conf can never had a
+# :jobid key
     if conf.has_key?(:jobid)
       parse_job_by_id conf
     end
   end
     
   # Submit our job... We'll need to play with defining the job owner, sudo for the qsub call, etc.
-  def submit
 
+  def submit
     # resource requests
     if self.hard_requests != nil
       reqs = Array.new
       self.hard_requests.keys.each { |cplx| reqs << cplx.to_s + "=" + self.hard_requests[cplx].to_s }
-      h_res_req = "-hard -l " + reqs * ","
+      #h_res_req = "-hard -l " + reqs * ","
+      h_res_req = reqs * " "
+      self.hard_resreq = Hash[*reqs.flatten]
     end
 
     if self.soft_requests != nil
       reqs = Array.new
       self.soft_requests.keys.each { |cplx| reqs << cplx.to_s + "=" + self.soft_requests[cplx].to_s }
-      s_res_req = "-soft -l " + reqs * ","
+#      s_res_req = "-soft -l " + reqs * ","
+      s_res_req = "-soft " + reqs * ","
     end
 
-    # build our command-line arguments from our accessor info
+  # build our command-line arguments from our accessor info
+#    cmdargs = Array.new
+#    cmdargs << h_res_req              if defined?(h_res_req)
+#    cmdargs << s_res_req              if defined?(s_res_req)
+#    cmdargs << "-j y"                 if self.joinout == true
+#    cmdargs << "-o #{self.outfile}"   if self.outfile != nil
+#    cmdargs << "-e #{self.errfile}"   if self.errfile != nil
+#    cmdargs << "-N #{self.job_name}"  if self.job_name != nil
+#    cmdargs << "-cwd"                 if self.cwd == true
+#    cmdargs << "-wd #{self.wd}"       if self.wd != nil
+#    cmdargs << "-q #{self.queue}"     if self.queue != nil
+#    cmdargs << "-pe #{self.pe} #{self.pe_slots}" if (self.pe != nil and self.pe_slots != nil)
+
     cmdargs = Array.new
-    cmdargs << h_res_req              if defined?(h_res_req)
-    cmdargs << s_res_req              if defined?(s_res_req)
-    cmdargs << "-j y"                 if self.joinout == true
-    cmdargs << "-o #{self.outfile}"   if self.outfile != nil
-    cmdargs << "-e #{self.errfile}"   if self.errfile != nil
-    cmdargs << "-N #{self.job_name}"  if self.job_name != nil
-    cmdargs << "-cwd"                 if self.cwd == true
-    cmdargs << "-wd #{self.wd}"       if self.wd != nil
-    cmdargs << "-q #{self.queue}"     if self.queue != nil
-    cmdargs << "-pe #{self.pe} #{self.pe_slots}" if (self.pe != nil and self.pe_slots != nil)
+    cmdargs << h_res_req                      if defined?(h_res_req)
+    cmdargs << s_res_req                      if defined?(s_res_req)
+    cmdargs << "-o #{self.outfile}"           if self.outfile != nil
+    cmdargs << "-e #{self.errfile}"           if self.errfile != nil
+    cmdargs << "--job-name=#{self.job_name}"  if self.job_name != nil
+#    cmdargs << "-cwd"                         if self.cwd == true
+    cmdargs << "--workdir=#{self.wd}"         if self.wd != nil
+    cmdargs << "--nodelist=#{self.queue}"     if self.queue != nil
+    cmdargs << "-n #{self.pe_slots}"          if self.pe_slots != nil
 
     # TODO: Implement the rest of the switches as hash keys/options
-
+    puts "execio sudo -u llschlit sbatch " + cmdargs * " ", self.script
     # we're going to submit a job
-    (status,error) = execio "sudo -u #{self.job_owner} qsub " + cmdargs * " ", self.script
+#   (status,error) = execio "sudo -u #{self.job_owner} qsub " + cmdargs * " ", self.script
+#     (status,error) = execio "sudo -u #{self.job_owner} sbatch " + cmdargs * " ", self.script
+    (status,error) = execio "/usr/bin/sudo -i -u #{self.job_owner} sbatch " + cmdargs * " ", self.script
 
     # check status
-    if status.class == String and status.match(/^Your job(\-array)* [0-9]+.*submitted$/)
+    if status.class == String and status.match(/^Submitted batch job [0-9]+$/)
       # TODO: :enumerate => :job is still acting funny.  SGE XML is hard to predict
       jobs = RsgeJobs.new self.job_owner
-      parse_job_by_id jobs.to_hash[status.match(/^Your job(\-array)* ([0-9]+).*submitted$/)[2]]
+      jobs = jobs.to_hash
+      #parse_job_by_id jobs.to_hash[status.match(/^Submitted batch job [0-9]+$/)[3]]
+      parse_job_by_id (jobs[status.match(/[0-9]+/).to_s])
       return 0 
     else
-      raise RuntimeError, "Job submission to SGE failed! => #{status} #{error}"
+    # raise RuntimeError, "Job submission to SGE failed! => #{status} #{error}"
+      raise RuntimeError, "Job submission to Slurm failed! => #{status} #{error}"
       return nil
     end
   end
   
   # Gotta be able to delete jobs, too
   def delete
-    (status,error) = execio "sudo -u #{self.job_owner} qdel #{self.jobid}", nil
-    if status.class == String and status.match(/^.*job.*#{self.jobid}.*/)
-      self.state = "d"
-      return 0
-    else
+#   (status,error) = execio "sudo -u #{self.job_owner} qdel #{self.jobid}", nil
+    (status,error) = execio "sudo -u #{self.job_owner} scancel #{self.jobid}", nil
+#    if status.class == String and status.match(/^.*job.*#{self.jobid}.*/)
+    if error.include? "scancel: error:" 
       raise RuntimeError, "Deletion of job #{self.jobid} failed!"
       return nil
-    end
+    else
+      self.state = "d"
+      puts "Job successfully deleted."
+      return 0
+   end
   end
 
   private
+#---------
+
   def parse_job_by_id(job)
-    # We're providing a job and its info
+  # We're providing a job and its info
 
     if job == nil
       self.valid = false
       return self
     end
 
-    self.jobid          = job[:jobid]
-    self.job_owner      = job[:job_owner]
-    self.job_name       = job[:job_name]
-    self.queue          = job[:queue]
-    self.queue_name     = job[:queue_name]
-    self.reservation    = job[:reservation] 
-    self.joinout        = job[:joinout]
-    self.state          = job[:state]
-    self.slots          = job[:slots]
-    self.hard_requests  = job[:hard_requests]
-    self.soft_requests  = job[:soft_requests]
+    self.jobid          = job[:jobid]           if job.has_key?(:jobid)
+    self.job_name       = job[:job_name]        if job.has_key?(:job_name)
+    self.slots          = job[:slots]           if job.has_key?(:slots)
+    self.submission_time= job[:submission_time] if job.has_key?(:submission_time)
+    self.start_time     = job[:start_time]      if job.has_key?(:start_time)
+    self.queue          = job[:queue]           if job.has_key?(:queue)
+    self.state          = job[:state]           if job.has_key?(:state)
+    self.job_owner      = job[:job_owner]       if job.has_key?(:job_owner)
+#    self.pe             = job[:pe]              if job.has_key?(:pe)
+#    self.pe_slots       = job[:pe_slots]        if job.has_key?(:pe_slots)
+    self.reservation    = job[:reservation]     if job.has_key?(:reservation)
+    self.joinout        = (self.errfile == nil ? true : false)
+    self.queue_name     = job[:queue_name]      if job.has_key?(:queue_name)
+    self.hard_requests  = job[:hard_requests]   if job.has_key?(:hard_requests)
+#    self.soft_requests  = job[:soft_requests]   if job.has_key?(:soft_requests)
 
-    #self.pe = conf[:pe]            if conf.has_key?(:pe)
-    #self.pe_slots = conf[:pe_slots]      if conf.has_key?(:pe_slots)
-    #self.script = conf[:script]        if conf.has_key?(:script)
+    self.script         = job[:script]          if job.has_key?(:script)
   end
 
 end
--- lib/rsgejobs.rb
+++ lib/rsgejobs.rb
@@ -11,6 +11,7 @@ include RsgeUtil
 # Provides methods for viewing GridEngine job information
 class RsgeJobs
   def initialize(user)
+=begin
     if $rspec_init == true
       doc = Nokogiri::XML(open("sample_data/job_list.xml"))
     else
@@ -24,7 +25,6 @@ class RsgeJobs
       state = node.attribute("state").to_s
 
       jobNumber = node.at_xpath(".//JB_job_number").to_str
-
       @@jobs[jobNumber] = Hash.new
       @@jobs[jobNumber][:hard_requests] = Hash.new
       @@jobs[jobNumber][:soft_requests] = Hash.new
@@ -55,8 +55,69 @@ class RsgeJobs
       end
     end
   end
+=end
 
-  # provide an RsgeJob for each job currently active
+  (s, error) = execio "squeue -u #{user} --noheader -o '%8i %3t %4p %6q %8P %15j %15u %10a %20V %20S %10M %11l %20B %4C %5D %5m %10v %10n %R'", nil
+
+  a = s.split(" ")
+  i = 0
+  @@jobs = Hash.new
+  jobNumber = 0
+
+  a.each do |elem|
+  # get the job number
+    if (i % 19 == 0)
+      jobNumber = elem.to_str
+      @@jobs[jobNumber] = Hash.new
+      @@jobs[jobNumber][:jobid] = jobNumber
+      @@jobs[jobNumber][:hard_requests] = Hash.new
+      i = 0
+    end
+
+    case i
+      when 1
+        @@jobs[jobNumber][:state] = elem
+      when 2
+        @@jobs[jobNumber][:priority] = elem
+      when 3
+        @@jobs[jobNumber][:qos] = elem
+      when 4
+        @@jobs[jobNumber][:partition] = elem
+      when 5
+        @@jobs[jobNumber][:job_name] = elem
+      when 6
+        @@jobs[jobNumber][:job_owner] = elem
+      when 7
+        @@jobs[jobNumber][:account] = elem
+      when 8
+        @@jobs[jobNumber][:submission_time] = elem
+      when 9
+        @@jobs[jobNumber][:start_time] = elem
+      when 10
+        @@jobs[jobNumber][:time] = elem
+      when 11
+        @@jobs[jobNumber][:hard_requests][:h_rt] = elem
+      when 12
+        @@jobs[jobNumber][:exec_host] = elem
+      when 13
+        @@jobs[jobNumber][:slots] = elem #aka cpus
+      when 14
+        @@jobs[jobNumber][:hard_requests][:nodes] = elem
+      when 15
+        @@jobs[jobNumber][:hard_requests][:min_m] = elem
+      when 16
+        @@jobs[jobNumber][:reservation] = elem
+      when 17
+        @@jobs[jobNumber][:queue] = elem
+      when 18
+        @@jobs[jobNumber][:nodelist_reason] = elem
+    end
+    i += 1
+  end
+
+end
+
+# provide an RsgeJob for each job currently active
   def each
     self.list.each do |jobid|
       yield RsgeJob.new @@jobs[jobid]
